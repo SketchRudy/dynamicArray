5e7419a86f208ea313908a9ba7a5e26a7dedd84c
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-04-08 12:02:57 -0700

    Test structure

--- src/main.java
+++ src/main.java
@@ -1,5 +1,5 @@
 public class Main implements StringList {
     public static void main (String[] args) {
-
+        
     }
 }
---
+++ src/mainTest.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+public class mainTest {
+    
+    @Test
+    void testReturnStringAtSpecifiedIndex() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void testReplacingStringAtGivenIndexWithGivenValue() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void testAddNewStringToEndOfList() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void testRemoveStringAtGivenIndex() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void testCurrentElementsInString   () {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+    @Test
+    void testMaxCapacityOfArray() {
+        // Arrange
+        // Act
+        // Assert
+    }
+
+
+}
