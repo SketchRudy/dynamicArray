223b692785e6771a81fc6f9304a241e19a8d5f46
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-04-08 12:08:51 -0700

    Added interface

--- src/main.java
+++ src/main.java
@@ -2,4 +2,45 @@ public class Main implements StringList {
     public static void main (String[] args) {
         
     }
+
+    @Overide
+    public void get() {
+        
+    }
+
+    @Override
+    public String get(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'get'");
+    }
+
+    @Override
+    public void set(int index, String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'set'");
+    }
+
+    @Override
+    public void add(String value) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public String remove(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'size'");
+    }
+
+    @Override
+    public int capacity() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'capacity'");
+    }
 }
--- src/mainTest.java
+++ src/mainTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-public class mainTest {
+public class MainTest {
     
     @Test
     void testReturnStringAtSpecifiedIndex() {
