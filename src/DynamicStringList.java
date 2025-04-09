81b0c8459c6031b9f4957a4ea4537e9cad8cc247
rjfredr <fredrickalexander1727@gmail.com>
2025-04-09 09:27:07 -0700

    made methods, unsure if correct will write testcases

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,32 +1,53 @@
 class DynamicStringList implements StringList{
-    String[] strList = new String[10];
+    String[] strList = new String[1];
     @Override
     public String get(int x) {
-        return "Test";
+        return strList[x];
     }
     @Override
     public void set(int x, String str) {
+        strList[x] = str;
     }
     @Override
     public void add(String str) {
+        try {
+            strList[strList.length-1] = str;
+        } catch (Exception e) {
+            String[] newStrList = new String[strList.length*2];
+            strList = newStrList;
+        }
     }
     @Override
     public String remove(int x) {
-        return "Test";
+        String removed = strList[x];
+        strList[x] = "";
+        return removed;
     }
     @Override
     public int size() {
-        return -1;
+        int size = 0;
+        for (String word : strList) {
+            size++;
+        }
+        return size;
     }
     @Override
     public int capacity() {
-        return -1;
+        return strList.length;
     }
 }
+//testing stuff without the @test, try to make stuff in maintest
+
 class Main {
     public static void main(String[] args) {
         DynamicStringList dsl = new DynamicStringList();
         dsl.add("RJ");
-        System.out.println();
+        System.out.println(dsl.get(0));
+        dsl.add("RJ");
+        System.out.println(dsl.get(0));
+        System.out.println(dsl.size());
+        for (int i = 0; i < dsl.capacity(); i++) {
+            System.out.println(dsl.get(i));
+        }
     }
 }
