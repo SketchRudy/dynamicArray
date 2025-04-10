471df36e4e1166c6ba9247e2d3de50fb477c694e
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-04-09 23:09:18 -0700

    Fixed minor errors

--- src/DynamicStringList.java
+++ src/DynamicStringList.java
@@ -1,5 +1,7 @@
 class DynamicStringList implements StringList{
     String[] strList = new String[1];
+    private int size = 0;
+
     @Override
     public String get(int x) {
         return strList[x];
@@ -8,29 +10,37 @@ class DynamicStringList implements StringList{
     public void set(int x, String str) {
         strList[x] = str;
     }
+
     @Override
     public void add(String str) {
-        try {
-            strList[strList.length-1] = str;
-        } catch (Exception e) {
+        if (size == strList.length) {
             String[] newStrList = new String[strList.length*2];
+            for (int i = 0; i < strList.length; i++) {
+                newStrList[i] = strList[i];
+            }
             strList = newStrList;
-        }
     }
+    strList[size] = str;
+    size++;
+    }
+
     @Override
     public String remove(int x) {
         String removed = strList[x];
-        strList[x] = "";
+        for (int i = x; i < size - 1; i++) {
+            strList[i] = strList[i+1];
+        }
+
+        strList[size - 1] = null;
+        size--;
         return removed;
     }
+
     @Override
     public int size() {
-        int size = 0;
-        for (String word : strList) {
-            size++;
-        }
         return size;
     }
+
     @Override
     public int capacity() {
         return strList.length;
